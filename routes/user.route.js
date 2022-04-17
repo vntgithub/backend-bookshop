0a7a326275d935bea850d1e0380b318b736964d6
Vo Nhat Trieu <vntrieu25879@gmail.com>
2022-04-17 13:06:23 +0700

    restart project

--- routes/user.route.js
+++ routes/user.route.js
@@ -2,14 +2,12 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controller/user.controller');
 
-router.get('/getbyid/:id', userController.getById);
-router.get('/getperpage/:page', userController.getUserPerPage);
-router.get('/count', userController.countUsers);
-router.get('/search/:username', userController.searchByUsername);
-router.post('/check', userController.checkUserExist);
+router.get('/:username', userController.checkUserExist);
+router.get('/index', userController.getUserPerPage);
+
 router.post('/create', userController.create);
 router.post('/login', userController.login);
-router.put('/update', userController.update);
-router.delete('/delete/:id', userController.deleteById);
+router.put('/:id', userController.update);
+router.delete('/:id', userController.deleteById);
 
 module.exports = router;
